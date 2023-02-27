ce4eaeeec7421a6ae78d8adcf45422b48a3ffec7
Тимур <119677187+Billiyherington@users.noreply.github.com>
2023-02-27 18:49:12 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// const BurgerWrap = document.querySelector(".burger-wrap")
+// const BurgerIcon = BurgerWrap.querySelector(".burger-icon")
+
+// BurgerWrap.addEventListener("click", ()=>{
+//     BurgerIcon.classList.toggle("active")
+// })
+
+
+
+document.addEventListener("keydown", ()=>{
+    console.log("нажал");
+})
